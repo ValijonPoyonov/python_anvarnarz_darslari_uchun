5695b1596abc3928aaaf08302cb8e55517eeda4b
ValijonPoyonov <valijonpoyonov@gmail.com>
2025-09-06 19:34:05 +0500

    Create 22_dars.py
    Navbatdagi darsning vazifalari

---
+++ 22_dars.py
@@ -0,0 +1,19 @@
+# Anvar Narzullayev video darslari. 22-dars vazifalari. MOSLASHUVCHAN FUNKSIYA(*args,**kwargs)  
+# 1-vazifa: Istalgancha sonlarni qabul qilib, ularning ko'paytmasini
+# qaytaruvchi funksiya yozing
+#def sonlarni_kopaytir(*sonlar):
+#    kopaytma =1
+#    for son in sonlar:
+#       kopaytma = kopaytma*son
+#   return kopaytma
+#print(sonlarni_kopaytir(1,2,3,4,5,6,7,8,9))
+
+
+# 2-vazifa: Talabalar haqidagi ma'lumotlarini lug'at ko'rinishida qaytaruvchi funkisya yozing.
+# Talabaning ismi va familiyasi majburiy argument, qolgan ma'lumotlar
+# esa ixtiyoriy ko'rinishda istalgancha berilishi mumkin bo'lsin.
+#def give_information(name,surname,**extra_info):
+#    extra_info['name']=name
+#    extra_info['surname']=surname
+#    return extra_info
+#print(give_information('valijon','poyonov',otasi='Nosirov Bahrom',onasi='Kuljanova Norhol',tugilgan_yili=1999,fakultet='mexanika',kurs=4))
